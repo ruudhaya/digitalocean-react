d6743d11b58e3d9ed654368cf7e41c1c8cf1ab6f
Udhay <udhayapsg@gmail.com>
2021-09-22 23:46:53 +0530

    There should be a single top level element

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css"
 
 function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <p>This is JSX</p>
+    </>
+  )
 }
 
 export default App
