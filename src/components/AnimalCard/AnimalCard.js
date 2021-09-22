3c47e1c21d791e8a9a7d9171e9364dde0738e7a0
Udhay <udhayapsg@gmail.com>
2021-09-23 01:26:28 +0530

    Sending props to a functional component

---
+++ src/components/AnimalCard/AnimalCard.js
@@ -0,0 +1,6 @@
+import "./AnimalCard.css"
+
+export default function AnimalCard(props) {
+  const { name } = props
+  return <h2>{name}</h2>
+}
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,41 +1,14 @@
 import "./App.css"
-import Instructions from "../Instructions/Instructions.js"
-
-const displayEmojiName = (event) => alert(event.target.id)
-const displayAction = false
-const emojis = [
-  {
-    name: "grinning face",
-    value: "😀",
-  },
-  {
-    name: "party popper",
-    value: "🎉",
-  },
-  {
-    name: "woman dancing",
-    value: "💃",
-  },
-]
+import Animals from "./data"
+import AnimalCard from "../AnimalCard/AnimalCard"
 
 function App() {
-  const greeting = "greeting"
   return (
-    <div className="container">
-      <h1 id={greeting}>Hello World!</h1>
-      {displayAction && <p>Doing JSX Action</p>}
-      <Instructions />
-      <ul>
-        {emojis.map((emoji) => (
-          <li>
-            <button onClick={displayEmojiName}>
-              <span role="img" aria-label={emoji.name} id={emoji.name}>
-                {emoji.value}
-              </span>
-            </button>
-          </li>
-        ))}
-      </ul>
+    <div className="wrapper">
+      <h1> Animals </h1>
+      {Animals.map((animal) => (
+        <AnimalCard key={animal.name} name={animal.name} />
+      ))}
     </div>
   )
 }
