7e8b46f89830276af2eb5468e43d3199446654d3
Udhay <udhayapsg@gmail.com>
2021-09-23 00:43:12 +0530

    Added img element in class component

--- src/Instructions.js
+++ src/Instructions.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react"
+import emoji from "./emoji.svg"
 
 export default class Instructions extends Component {
   render() {
-    return <p>Click on the emoji to see their name</p>
+    return (
+      <div className="instructions">
+        <img src={emoji} alt="laughing crying emoji" />
+        <p>Click on the emoji to see their name</p>
+      </div>
+    )
   }
 }
