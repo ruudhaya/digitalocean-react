3613c51d5f31f6206fdbf206a1aa2d4d50e4e822
Udhay <udhayapsg@gmail.com>
2021-09-23 00:33:33 +0530

    Used the class component in another component

--- src/Instructions.js
+++ src/Instructions.js
@@ -1,3 +1,7 @@
 import React, { Component } from "react"
 
-export default class Instructions extends Component {}
+export default class Instructions extends Component {
+  render() {
+    return <p>Click on the emoji to see their name</p>
+  }
+}
