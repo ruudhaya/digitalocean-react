2f881ffeb6f9f859045fb00b95754104a008c069
Udhay <udhayapsg@gmail.com>
2021-09-22 23:56:48 +0530

    Added semantic attributes to span element

--- src/App.js
+++ src/App.js
@@ -6,6 +6,29 @@ function App() {
     <div className="container">
       <h1 id={greeting}>Hello World!</h1>
       <p>This is JSX</p>
+      <ul>
+        <li>
+          <button>
+            <span role="img" aria-label="grinning face" id="grinning face">
+              😀
+            </span>
+          </button>
+        </li>
+        <li>
+          <button>
+            <span role="img" aria-label="party popper" id="party popper">
+              🎉
+            </span>
+          </button>
+        </li>
+        <li>
+          <button>
+            <span role="img" aria-label="woman dancing" id="woman dancing">
+              💃
+            </span>
+          </button>
+        </li>
+      </ul>
     </div>
   )
 }
