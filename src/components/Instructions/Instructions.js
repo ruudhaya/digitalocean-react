1e2f9e20c292857a07e9fa914bebb01b3e389b8b
Udhay <udhayapsg@gmail.com>
2021-09-23 01:06:30 +0530

    Simple class component converted to Functional component

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -1,14 +1,12 @@
-import React, { Component } from "react"
+import React from "react"
 import emoji from "./emoji.svg"
 import "./Instructions.css"
 
-export default class Instructions extends Component {
-  render() {
-    return (
-      <div className="instructions">
-        <img src={emoji} alt="laughing crying emoji" />
-        <p>Click on the emoji to see their name</p>
-      </div>
-    )
-  }
+export default function Instructions() {
+  return (
+    <div className="instructions">
+      <img src={emoji} alt="laughing crying emoji" />
+      <p>Click on the emoji to see their name</p>
+    </div>
+  )
 }
